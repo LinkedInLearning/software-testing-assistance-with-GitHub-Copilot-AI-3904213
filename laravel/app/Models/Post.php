1ddb8fb976c70531e37babca7a1af0f3d5baf826
Gary Kovar <gkovar@gmail.com>
2024-05-22 00:16:03 +0000

    adds properties

--- laravel/app/Models/Post.php
+++ laravel/app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'author_id',
+    ];
 }
