f6157d3365f98d90751968a0de898a487522c8b8
Gary Kovar <gkovar@gmail.com>
2024-05-22 00:19:35 +0000

    end state

--- example.php
+++ example.php
@@ -4,3 +4,18 @@
 function add($a, $b) {
     return $a + $b;
 }
+
+function alphabetize_by_last_name( array $names ) {
+    usort( $names, function( $a, $b ) {
+        $a = explode( ' ', $a );
+        $b = explode( ' ', $b );
+        return strcasecmp( end( $a ), end( $b ) );
+    } );
+    return $names;
+}
+
+// validate email address 
+function validate_email($email) {
+  // regex validate email address
+  return preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $email);
+}
