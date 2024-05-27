8d534d416431f7ac42615d0c73d0bbb2340dcaf7
Gary Kovar <gkovar@gmail.com>
2024-05-27 17:38:09 +0000

    start state

---
+++ link_presenter.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpParser\Node\Expr\FuncCall;
+
+$example_link_array_1 = [
+  'id' => 1,
+  'url' => 'https://www.linkedin.com',
+];
+
+$example_link_array_2 = [
+  'id' => false,
+  'url' => 'learning',
+];
+
+$example_link_array_3 = [
+  'id' => false,
+  'url' => 'https://www.linkedin.com',
+];
