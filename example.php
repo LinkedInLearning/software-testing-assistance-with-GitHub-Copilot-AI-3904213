498a711343d3c954ca97303d471ff8b69cabcdef
Gary Kovar <gkovar@gmail.com>
2024-05-27 17:18:56 +0000

    start with slugify command

--- example.php
+++ example.php
@@ -21,3 +21,22 @@ function validate_email($email) {
   // regex validate email address
   return preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $email);
 }
+
+function slugify( $text ) {
+    // replace non letter or digits by -
+    $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+    // transliterate
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    // remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
+    // trim
+    $text = trim($text, '-');
+    // remove duplicate -
+    $text = preg_replace('~-+~', '-', $text);
+    // lowercase
+    $text = strtolower($text);
+    if (empty($text)) {
+        return 'n-a';
+    }
+    return $text;
+}
