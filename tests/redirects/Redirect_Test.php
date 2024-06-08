8f2630576c9ae9e514144116a670966e7a2fbefa
Gary Kovar <gkovar@gmail.com>
2024-06-08 17:26:06 +0000

    end state

--- tests/redirects/Redirect_Test.php
+++ tests/redirects/Redirect_Test.php
@@ -14,11 +14,36 @@ class Redirect_Test extends TestCase
         parent::setUp();
     }
 
-    /**
-     * Test that the function `redirect` exists.
-     */
-    public function test_function_exists(): void
+    public function test_basic_redirect()
     {
-        $this->assertTrue(function_exists('redirect'));
+        $this->assertEquals(true, should_redirect('lil'));
+        $this->assertEquals( 'https://www.linkedin.com', redirect_value('lil'));
     }
+
+    public function test_should_redirect() {
+        $this->assertEquals(true, should_redirect('lil'));
+        $this->assertEquals(true, should_redirect('learning'));
+        $this->assertEquals(false, should_redirect('lillearning'));
+    }
+
+    public function test_redirect_value() {
+        $this->assertEquals('https://www.linkedin.com', redirect_value('lil'));
+        $this->assertEquals('https://www.linkedin.com/learning', redirect_value('learning'));
+        $this->assertEquals(null, redirect_value('lillearning'));
+    }
+
+    public function test_invalid_redirects_should_not_redirect() {
+        $this->assertEquals(false, should_redirect('lillearning'));
+        $this->assertEquals(null, redirect_value('foo'));
+    }
+
+    public function test_redirect_with_trailing_slash() {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+
+    public function test_with_query_args() {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+    
+
 }
